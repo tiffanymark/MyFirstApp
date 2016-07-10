25bca02abaf9d57e876d644f20578b15bbe4746b
Tiffany Mark <tiffmark@hotmail.com>
2016-07-10 12:56:17 -0300

    lógica de negócio foi passada da view para o controller

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,6 +1,12 @@
 class GreeterController < ApplicationController
   def hello
+    random_names = ["Tiffany", "Thiago", "Stephanie"]
+    @name = random_names.sample
+    @time = Time.now
   end
   def goodbye
+    random_names = ["Tiffany", "Thiago", "Stephanie"]
+    @name = random_names.sample
+    @time = Time.now
   end
 end
